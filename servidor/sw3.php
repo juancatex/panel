1293bc5505b8d9a969e3375cda4a46b0ded5b609
Ing.Juan carlos (Casa) <juan_carlos1345@hotmail.com>
2020-09-04 08:33:39 -0400

    ok

--- servidor/sw3.php
+++ servidor/sw3.php
@@ -2,11 +2,11 @@
     $valores = array(); 
     $titulo = 'Aniversario LXIII de Creacion de Asociacion Nacional de Suboficiales y Sargentops de las Fuerzas Armadas';
     $contenido = 'Ceremonia de conmemoracion al LXIII de Creacion de Asociacion Nacional de Suboficiales y Sargentops de las Fuerzas Armadas, realizado el pasado viernes 4 de octubre';
-    $fotos = array (array('image' => 'http://www.ascinalss.org/ascinalss/movil/social/ani01.jpg'),
-                    array('image' => 'http://www.ascinalss.org/ascinalss/movil/social/ani02.jpg'),
-                    array('image' => 'http://www.ascinalss.org/ascinalss/movil/social/ani03.jpg'),
-                    array('image' => 'http://www.ascinalss.org/ascinalss/movil/social/ani04.jpg'),
-                    array('image' => 'http://www.ascinalss.org/ascinalss/movil/social/ani05.jpg'),
+    $fotos = array (array('image' => 'https://ascinalss.herokuapp.com/social/ani01.jpg'),
+                    array('image' => 'https://ascinalss.herokuapp.com/social/ani02.jpg'),
+                    array('image' => 'https://ascinalss.herokuapp.com/social/ani03.jpg'),
+                    array('image' => 'https://ascinalss.herokuapp.com/social/ani04.jpg'),
+                    array('image' => 'https://ascinalss.herokuapp.com/social/ani05.jpg'),
     ); 
     $valores = array('titulo'=>$titulo, 'contenido'=>$contenido, 'fotos'=>$fotos); 
     echo json_encode($valores); 
