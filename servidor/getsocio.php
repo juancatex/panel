29d185a645f219954b07d137025fc3aa7dcf280c
Ing.Juan carlos (Casa) <juan_carlos1345@hotmail.com>
2021-01-21 16:12:41 -0400

    ok

--- servidor/getsocio.php
+++ servidor/getsocio.php
@@ -1,15 +1,15 @@
 <?php 
- if(!empty($_GET["num"])){ 
+ if(!empty($_POST["num"])){ 
    require_once 'include/db_handler_apk.php';  
     				$db = new DbHandler();   
-					$salida = $db->getstatussocio($_GET["num"]); 
+					$salida = $db->getstatussocio($_POST["num"]); 
 					 $db->close();
 		 if($salida==null)			 
         echo json_encode(array ('value'=>0));  
        else
 	    echo json_encode(array ('value'=>1,'data'=>$salida));  
     }else{ 
-	  echo json_encode(array ('value'=>100));  	
+	  echo json_encode(array ('value'=>0));  	
 	}
 	 
 ?>
